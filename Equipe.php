3e52301ec508bebd5c93702f680072b4035ef564
Mamath68 <mathieu.stamm@gmail.Com>
2023-03-06 14:57:59 +0100

    modif 15H

--- Equipe.php
+++ Equipe.php
@@ -5,10 +5,45 @@ class Equipe
 
     private $name;
     private $dateCreation;
-    private Pays $contry;
+    private Pays $country;
 
-    public function __construct($name, $dateCreation, $contry)
+    public function __construct($name, $dateCreation, $country)
     {
+        $this->name = $name;
+        $this->dateCreation = $dateCreation;
+        $this->country = $country;
 
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+    public function getDateCreation()
+    {
+        return $this->dateCreation;
+    }
+    public function getCountry()
+    {
+        return $this->country;
+    }
+
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+    public function setDateCreation($dateCreation)
+    {
+        $this->dateCreation = $dateCreation;
+    }
+    public function setCountry($country)
+    {
+        $this->contry = $country;
+    }
+
+    public function __toString()
+    {
+        return "OK";
     }
 }
--- Joueur.php
+++ Joueur.php
@@ -5,12 +5,62 @@ class Joueur
     private $firstName;
     private $name;
     private $bDays;
-    private Pays $country;
+    private $country;
     private Equipe $team;
 
     public function __construct($firstName, $name, $bDays, $country, $team)
     {
-
+        $this->firstName = $firstName;
+        $this->name = $name;
+        $this->bDays = $bDays;
+        $this->country = $country;
+        $this->team = $team;
     }
 
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+    public function getName()
+    {
+        return $this->name;
+    }
+    public function getBdays()
+    {
+        return $this->bDays;
+    }
+    public function getCountry()
+    {
+        return $this->country;
+    }
+    public function getTeam()
+    {
+        return $this->team;
+    }
+
+    public function setFirstName($firstName)
+    {
+        $this->firstName = $firstName;
+    }
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+    public function setBdays($bDays)
+    {
+        $this->bDays = $bDays;
+    }
+    public function setCountry($country)
+    {
+        $this->country = $country;
+    }
+    public function setTeam($team)
+    {
+        $this->team = $team;
+    }
+
+    public function __toString()
+    {
+        return "ok";
+    }
 }
